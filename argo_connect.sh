90789e4b055dcfb7a69ae6fbc2885366b7485f0a
Marcin Jasiukowicz <hi@y4s.io>
2023-09-09 16:16:55 +0200

    UX changes

--- argo_connect.sh
+++ argo_connect.sh
@@ -4,6 +4,7 @@
 jumphost=argo@argo
 device=$1
 service=$2
+command=$3
 network_prefix="192.168.0."
 isIDRAC=false
 identity_file=./argo.private
@@ -14,10 +15,10 @@ IDRAC_PASSWORD=calvin
 ## Get IP
 if [ -z $device ]; then
     echo "Options:"
-    echo -e "1-16) \t\t Connect to blades iDRAC {ssh, web, kvm} \t $0 [1-16] {ssh, web, kvm}"
-    echo -e "chassis) \t Connect to CMC {ssh, web} \t\t\t $0 chassis {ssh, web}"
-    echo -e "network) \t Connect to network switch {ssh, web} \t\t $0 network {ssh, web-insecure}"
-    echo -e "q) \t\t Quit"
+    echo -e "1-16 \t\t -- connect to blades iDRAC {ssh, web, kvm}"
+    echo -e "chassis \t -- connect to CMC {ssh, web}"
+    echo -e "network \t -- connect to network switch {ssh, web}"
+    echo -e "q \t\t -- quit"
     read -p "Selection: " device
 fi
 
@@ -42,10 +43,14 @@ esac
 ## Run Proxy
 if [ -f $identity_file ]; then
     echo Connecting to $ip via $jumphost
+    # Kill all already running proxies
+    ps aux | grep $jumphost | grep -v grep | awk '{print $2}' | xargs kill -9 &
+    sleep 0.1
+    # Open a new proxy
     ssh -i $identity_file -N -L 8443:$ip:443 -L 8022:$ip:22 -L 8080:$ip:80 -L 5900:$ip:5900 -L 5901:$ip:5901 -L 3668:$ip:3668 -L 3669:$ip:3669 $jumphost &
     ssh_process=$!
-    sleep 1
-    echo $ssh_process
+    sleep 0.5
+    echo Started proxy \($ssh_process\)
 else   
     echo "SSH identity not found, cannot open Proxy. Exiting..."
     exit
@@ -54,12 +59,12 @@ fi
 ## Run Service
 if [ -z $service ]; then
     echo Options: 
-    echo -e "kvm) \t\t Open JavaKVM"
-    echo -e "ssh) \t\t Open SSH connection"
-    echo -e "web) \t\t Open https://localhost:8443"
-    echo -e "web-insecure) \t Open http://localhost:8080"
-    echo -e "proxy) \t\t Just proxy, don't open anything"
-    echo -e "q) \t\t Quit"
+    echo -e "kvm \t\t -- open JavaKVM"
+    echo -e "ssh \t\t -- open SSH connection"
+    echo -e "web \t\t -- open https://localhost:8443"
+    echo -e "web-insecure \t -- open http://localhost:8080"
+    echo -e "proxy \t\t -- just proxy, don't open anything"
+    echo -e "q \t\t -- quit"
     read -p "Selection: " service
 fi
 
@@ -72,12 +77,12 @@ case $service in
             export IDRAC_HOST=localhost:8443
             ./bootleg-idrac6-client/kvm.sh
         else
-            echo "Unsupported option"
+            echo "$service - unsupported option"
         fi
     ;;
     "ssh")
         echo Starting SSH
-        ssh $IDRAC_USER@localhost -i $identity_file -p 8022 -o "StrictHostKeyChecking=no" -o "KexAlgorithms=+diffie-hellman-group14-sha1,diffie-hellman-group1-sha1" -o "Ciphers=+3des-cbc" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -o "HostkeyAlgorithms=+ssh-rsa"
+        ssh $IDRAC_USER@localhost -i $identity_file -p 8022 -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "KexAlgorithms=+diffie-hellman-group14-sha1,diffie-hellman-group1-sha1" -o "Ciphers=+3des-cbc" -o "PubkeyAcceptedAlgorithms=+ssh-rsa" -o "HostkeyAlgorithms=+ssh-rsa" $command
         #ssh -i $identity_file -J $jumphost $IDRAC_USER@$ip
     ;;
     "web")
@@ -96,11 +101,10 @@ case $service in
         read -p "Press any key to exit"
     ;;
     *)
-        echo "Unsupported option"
+        echo "Quitting..."
     ;;
 esac
 
 
 kill -9 $ssh_process
 echo Killed proxy \($ssh_process\) 
-ps aux | grep $jumphost | awk 'NR==1{print $2}' | xargs kill -9
